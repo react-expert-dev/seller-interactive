c3986759ed9377554d154567353e5a7fa30b7c58
ziaurrehman123 <ziaurrehman1122111@gmail.com>
2021-10-17 11:45:42 +0500

    make contact us section component

--- src/Pages/HairBurst/index.js
+++ src/Pages/HairBurst/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Box, Grid, Typography, OutlinedInput } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import ContactUsSection from "../../Components/ContactUsSection";
 const HairBurst = () => {
   return (
     <Box sx={{ flexGrow: 1, marginTop: 10 }}>
@@ -203,68 +204,7 @@ const HairBurst = () => {
         listing images are pleasing to the eye, and both are holistically sales
         effective.
       </Typography>
-      <Typography variant="h3" sx={{ fontWeight: 800, marginTop: 10, textAlign:"center" }}>
-      Want to be another success story?
-      </Typography>
-      <Grid container spacing={1} sx={{ marginBottom: 10 }}>
-          <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Typography
-              variant="h4"
-              gutterBottom
-              component="div"
-              sx={{
-                marginTop: 5,
-                fontWeight: 800,
-              }}
-            >
-              Contact Us.
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom component="div">
-              Let us get to know you better.
-            </Typography>{" "}
-            <Typography
-              variant="subtitle1"
-              gutterBottom
-              component="div"
-              sx={{ marginTop: -1 }}
-            >
-              Book a free Amazon Seller account audit with our consultants.
-            </Typography>{" "}
-            <Typography variant="h6" gutterBottom component="div" sx={{marginTop:3}}>
-              During your audit our consultants will:
-            </Typography>
-            <ul style={{ lineHeight: 1.5 }}>
-              <li>Discuss ways we can meet your business goals</li>
-              <li>Perform an Amazon brand and competitor analysis</li>
-              <li>Show how our expertise can help you kickstart your sales</li>
-            </ul>
-          </Grid>
-
-          <Grid
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={4}
-            sx={{
-              marginTop: 8,
-            }}
-          >
-            <Typography variant="h6" gutterBottom component="div">
-              Email *
-            </Typography>
-            <OutlinedInput placeholder="Enter email" sx={{ width: "100%" }} />
-            <Button
-              variant="contained"
-              sx={{
-                marginTop: 3,
-              }}
-            >
-              Get in touch
-            </Button>
-          </Grid>
-          <Grid item xs={2}></Grid>
-        </Grid>
+     <ContactUsSection />
     </Box>
   );
 };
